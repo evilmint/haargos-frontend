8c7594bfe8777394dd9ef08f742fc3249bc744aa
Aleksander Lorenc <thaxsillion@gmail.com>
2023-07-29 07:09:05 +0200

    Add agent version.

---
+++ components/ui/SVGWithText.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export function SVGWithText({ ...props }) {
+    const { showSVG, textWithSVG, fallbackText } = props;
+  
+    return (
+      <div>
+        {showSVG ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="h-4 w-4 text-muted-foreground inline mr-2"
+          >
+            <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path>
+            <line x1="12" y1="9" x2="12" y2="13"></line>
+            <line x1="12" y1="17" x2="12.01" y2="17"></line>
+          </svg>
+        ) : null}
+        <span className="inline">{showSVG ? textWithSVG : fallbackText}</span>
+      </div>
+    );
+  };
--- components/ui/docker.tsx
+++ components/ui/docker.tsx
@@ -24,13 +24,17 @@ import {
   CardTitle,
 } from "@/registry/new-york/ui/card";
 
+import { SVGWithText } from "./SVGWithText";
+
 export function Docker({ ...props }) {
   const { observation } = props;
 
   return (
     <Card className="col-span-8">
       <CardHeader>
-        <CardTitle>Docker containers ({observation.docker.containers.length})</CardTitle>
+        <CardTitle>
+          Docker containers ({observation.docker.containers.length})
+        </CardTitle>
       </CardHeader>
       <CardContent className="pl-2">
         <Table>
@@ -53,10 +57,24 @@ export function Docker({ ...props }) {
                   {container.name}
                 </TableCell>
                 <TableCell className="text-xs">{container.image}</TableCell>
-                <TableCell className="text-xs">{container.running ? "Yes" : "No"}</TableCell>
-                <TableCell className="text-xs">{container.restarting ? "Yes" : "No"}</TableCell>
-                <TableCell className="text-xs">{container.started_at}</TableCell>
-                <TableCell className="text-xs">{container.finished_at}</TableCell>
+                <TableCell className="text-xs">
+                  {container.running ? "Yes" : "No"}
+                </TableCell>
+                <TableCell className="text-xs">
+                 <SVGWithText
+                    showSVG={container.restarting}
+                    textWithSVG="Yes"
+                    fallbackText="No"
+                  />
+                </TableCell>
+                <TableCell className="text-xs">
+                  {new Date(container.started_at).toLocaleString()}
+                </TableCell>
+                <TableCell className="text-xs">
+                  {new Date(container.finished_at).getUTCSeconds() > 0
+                    ? new Date(container.finished_at).toLocaleString()
+                    : "-"}
+                </TableCell>
                 <TableCell className="text-xs">{container.state}</TableCell>
                 <TableCell className="text-xs">{container.status}</TableCell>
               </TableRow>

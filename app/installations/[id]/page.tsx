9240cf36b2c717473eb83a50c211d22645f3739e
Aleksander Lorenc <thaxsillion@gmail.com>
2023-07-31 11:59:35 +0200

    Try

--- app/installations/[id]/page.tsx
+++ app/installations/[id]/page.tsx
@@ -1,10 +1,4 @@
 import Image from "next/image";
-
-// import { Logs } from "@/components/ui/logs";
-// import { Storage } from "@/components/ui/storage";
-// import { Docker } from "@/components/ui/docker";
-// import { Environment } from "@/components/ui/environment";
-
 import {
   Card,
   CardContent,
@@ -12,21 +6,14 @@ import {
   CardHeader,
   CardTitle,
 } from "@/registry/new-york/ui/card";
-
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/registry/new-york/ui/tabs";
-
+import { Tabs, TabsContent } from "@/registry/new-york/ui/tabs";
 import { MainNav } from "@/components/ui/main-nav";
+import { Overview } from "@/components/ui/overview";
 import { Installations } from "@/components/ui/installations";
+import { DashboardHeader } from "@/components/ui/dashboardHeader";
 import { UserNav } from "@/components/ui/user-nav";
-// import { DashboardHeaderInstallation } from "@/components/ui/dashboardHeaderInstallation";
 
-export default function DashboardInstallationPage({ params }: { params: { id: string } }) {
-
+export default function DashboardPage() {
   return (
     <>
       <div className="md:hidden">
@@ -56,38 +43,18 @@ export default function DashboardInstallationPage({ params }: { params: { id: st
         </div>
         <div className="flex-1 space-y-4 p-8 pt-6">
           <Tabs defaultValue="overview" className="space-y-4">
-            <TabsList>
-              <TabsTrigger value="overview">Overview</TabsTrigger>
-              <TabsTrigger value="environment">Environment</TabsTrigger>
-              <TabsTrigger value="docker">Docker</TabsTrigger>
-              <TabsTrigger value="notifications" disabled>
-                Notifications
-              </TabsTrigger>
-            </TabsList>
-
             <TabsContent value="overview" className="space-y-4">
-              {/* <DashboardHeaderInstallation /> */}
+              <DashboardHeader />
               <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-                <Card className="col-span-7">
+                <Card className="col-span-4">
                   <CardHeader>
-                    <CardTitle>Installations</CardTitle>
-                    <CardDescription>Client installations</CardDescription>
+                    <CardTitle>Issues</CardTitle>
                   </CardHeader>
-                  <CardContent>
-                    <Installations />
-                  </CardContent>
-                </Card>
-                <Card className="col-span-8">
                   <CardContent className="pl-2">
-                    {/* <Logs installationId={params.id} /> */}
+                    <Overview />
                   </CardContent>
                 </Card>
-              </div>
-            </TabsContent>
-{/* 
-            <TabsContent value="environment" className="space-y-4">
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-                <Card className="col-span-7">
+                <Card className="col-span-3">
                   <CardHeader>
                     <CardTitle>Installations</CardTitle>
                     <CardDescription>Client installations</CardDescription>
@@ -96,35 +63,8 @@ export default function DashboardInstallationPage({ params }: { params: { id: st
                     <Installations />
                   </CardContent>
                 </Card>
-
-                <Card className="col-span-7">
-                  <CardHeader>
-                    <CardTitle>CPU</CardTitle>
-                  </CardHeader>
-                  <CardContent>
-                    <Environment />
-                  </CardContent>
-                </Card>
-
-                <Storage />
               </div>
             </TabsContent>
-
-            <TabsContent value="docker" className="space-y-4">
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-                <Card className="col-span-7">
-                  <CardHeader>
-                    <CardTitle>Installations</CardTitle>
-                    <CardDescription>Client installations</CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <Installations />
-                  </CardContent>
-                </Card>
-
-                <Docker />
-              </div>
-            </TabsContent> */}
           </Tabs>
         </div>
       </div>
